ea8a427d30b10abef6e3ee2a407737c358f2a315
andreivasyliev <andrew@foxtrapp.com>
2020-02-09 19:10:39 +0200

    upload

---
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -0,0 +1,41 @@
+package ru.deliveryClub;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FirstTest extends WebDriverSettings{
+
+
+    @Test
+    public void firstTest() {
+
+        String title = driver.getTitle();
+
+        Assert.assertTrue(title.equals("Джин - анонімний пошук роботи для розробників \\m/"));
+
+    }
+
+    @Test
+    public void login() throws InterruptedException{
+
+        WebElement navbar = driver.findElementByClassName("navbar");
+
+        navbar.findElement(By.cssSelector("[href=\"/login\"]")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type=\"email\"]")));
+
+        WebElement email = driver.findElementByCssSelector("[type=\"email\"]");
+
+        email.sendKeys("bob@yopmail.com");
+
+        Thread.sleep(3000);
+
+    }
+
+}
---
+++ src/test/java/ru/deliveryClub/WebDriverSettings.java
@@ -0,0 +1,26 @@
+package ru.deliveryClub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSettings {
+
+    public ChromeDriver driver;
+
+    @Before
+    public void setUp() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/andreivasyliev/Downloads/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.get("https://djinni.co/");
+
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
+
+}
